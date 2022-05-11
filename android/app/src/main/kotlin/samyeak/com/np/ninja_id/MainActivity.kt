37ffbf6ddb38bdb1d19f3d8f2881c27b640e60b1
Samyeak Maharjan <mail@samyeak.com.np>
2022-05-11 06:59:55 +0545

    Learned and implemented stateless widgets and other basic widgets

---
+++ android/app/src/main/kotlin/samyeak/com/np/ninja_id/MainActivity.kt
@@ -0,0 +1,6 @@
+package samyeak.com.np.ninja_id
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: const NinjaCard(),
+  ));
+}
+
+class NinjaCard extends StatelessWidget {
+  const NinjaCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        title: Text("Ninja ID Card"),
+        centerTitle: true,
+        backgroundColor: Colors.grey[800],
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 30, vertical: 40),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage("assets/thumb.jpg"),
+                radius: 40,
+              ),
+            ),
+            Divider(
+              height: 60,
+              color: Colors.grey[500],
+            ),
+            Text(
+                "NAME",
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(height: 10,),
+            Text(
+              "Samyeak Maharjan",
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                letterSpacing: 2.0,
+                fontSize: 30,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+            SizedBox(height: 30,),
+            Text(
+                "CURRENT NINJA LEVEL",
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(height: 10,),
+            Text(
+              "999",
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                letterSpacing: 2.0,
+                fontSize: 30,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+            SizedBox(height: 30,),
+            Row(
+              children: [
+                Icon(Icons.email_outlined, color: Colors.grey[400]),
+                SizedBox(width: 10),
+                Text(
+                  "mail@samyeak.com.np",
+                  style: TextStyle(
+                    color: Colors.grey[400],
+                    fontSize: 18,
+                    letterSpacing: 1
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
